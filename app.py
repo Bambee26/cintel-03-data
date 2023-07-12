f1a275863015742469be24a8eb29716818fbeb9a
Bambee Garfield <bambeegarfield@gmail.com>
2023-07-12 12:51:24 -0700

    Updating theme

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ from util_logger import setup_logger
 logger, logname = setup_logger(__name__)
 
 app_ui = ui.page_navbar(
-    shinyswatch.theme.slate(),
+    shinyswatch.theme.cerulean(),
     ui.nav(
         "Home",
         ui.layout_sidebar(
