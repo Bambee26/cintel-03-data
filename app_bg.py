d1fb770e0144b3a41671a63b9fd4460c565fd359
Bambee Garfield <bambeegarfield@gmail.com>
2023-07-12 10:54:33 -0700

    Test

--- app_bg.py
+++ app_bg.py
@@ -19,9 +19,9 @@ from penguins_server import get_penguins_server_functions
 from penguins_ui_inputs import get_penguins_inputs
 from penguins_ui_outputs import get_penguins_outputs
 
-from iris_server import get_iris_server_functions
-from iris_ui_inputs import get_iris_inputs
-from iris_ui_outputs import get_iris_outputs
+#from iris_server import get_iris_server_functions
+#from iris_ui_inputs import get_iris_inputs
+#from iris_ui_outputs import get_iris_outputs
 
 from util_logger import setup_logger
 
@@ -35,11 +35,11 @@ app_ui = ui.page_navbar(
             ui.panel_sidebar(
                 ui.h2("Sidebar Panel"),
                 ui.tags.hr(),
-                ui.h3("User Interaction Here"),
+                ui.h3("Let's get to know you!"),
                 ui.input_text("name_input", "Hi! What's your name?", placeholder="Your Name"),
                 ui.input_text(
                     "language_input",
-                    "What are your favorite language(s)",
+                    "What are your favorite language(s)?",
                     placeholder="Fave Language(s)",
                 ),
                 ui.tags.hr(),
@@ -58,12 +58,9 @@ app_ui = ui.page_navbar(
                         "To explore the Iris Dataset, click the 'Iris' tab."
                     ),
                 ),
-                ui.tags.hr(),
                 ui.h2("Main Panel with Reactive Output"),
-                ui.tags.hr(),
                 ui.output_text_verbatim("welcome_output"),
                 ui.output_text_verbatim("insights_output"),
-                ui.tags.hr(),
             ),
         ),
     ),
@@ -81,13 +78,13 @@ app_ui = ui.page_navbar(
             get_penguins_outputs(),
         ),
     ),
-    ui.nav(
-        "Iris",
-        ui.layout_sidebar(
-            get_iris_inputs(),
-            get_iris_outputs(),
-        ),
-    ),
+    #ui.nav(
+        #"Iris",
+        #ui.layout_sidebar(
+            #get_iris_inputs(),
+           # get_iris_outputs(),
+        #),
+    #),
     ui.nav(ui.a("About", href="https://github.com/bambee26")),
     ui.nav(ui.a("GitHub", href="https://github.com/bambee26/cintel-03-data")),
     ui.nav(ui.a("App", href="https://bambee26.shinyapps.io/cintel-03-data/")),
@@ -104,7 +101,7 @@ def server(input, output, session):
     @render.text
     def welcome_output():
         user = input.name_input()
-        welcome_string = f"Greetings {user}!"
+        welcome_string = f'Hi {user}, welcome!'
         return welcome_string
 
     @output
@@ -112,11 +109,11 @@ def server(input, output, session):
     def insights_output():
         answer = input.language_input()
         count = len(answer)
-        language_string = f"You like {answer}. That takes {count} characters"
+        language_string = f'You like {answer}. That takes {count} characters'
         return language_string
 
     get_mtcars_server_functions(input, output, session)
     get_penguins_server_functions(input, output, session)
-    get_iris_server_functions(input, output, session)
+    #get_iris_server_functions(input, output, session)
 
 app = App(app_ui, server)
